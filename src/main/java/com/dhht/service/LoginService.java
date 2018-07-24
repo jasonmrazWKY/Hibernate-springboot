9b5f7a1e72bf0fdd8e59d4faaf475119066078c2
WangkaiYuan <948353760@qq.com>
2018-07-24 09:32:22 +0800

    ddd

---
+++ src/main/java/com/dhht/service/LoginService.java
@@ -0,0 +1,13 @@
+package com.dhht.service;
+
+import org.springframework.stereotype.Service;
+
+import com.dhht.common.AccessResult;
+
+@Service
+public class LoginService {
+    
+	public AccessResult login(String userName,String password) {
+		return null;
+	}
+}
