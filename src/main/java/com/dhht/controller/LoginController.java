23259e1199b2602d0b1173d29dd207f2424edc87
WangkaiYuan <948353760@qq.com>
2018-07-24 10:03:53 +0800

    sas

--- src/main/java/com/dhht/controller/LoginController.java
+++ src/main/java/com/dhht/controller/LoginController.java
@@ -1,18 +1,28 @@
 package com.dhht.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.dhht.common.AccessResult;
 import com.dhht.entity.User;
+import com.dhht.service.LoginService;
 
 @Controller
 public class LoginController {
+	
+	@Autowired
+	private LoginService loginservice;
    
 	@RequestMapping("login")
 	public String checkLogin(User user){
 		System.out.println(user.getName()+"\t"+user.getPassword());
-		return "hello";
+		AccessResult ac = loginservice.login(user.getName(), user.getPassword());
+		if(ac.isSuccess()) {
+			return "success";
+		}
+		return "index";
 	}
 }
